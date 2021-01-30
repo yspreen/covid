5d17e0e4bd8bf63dec3315c889e5ed6567a0bb57
Yannick Spreen <yannick.spreen@gmail.com>
2021-01-30 20:42:58 +0100

    Init

---
+++ dist/precache-manifest.875ce4ab3177a8d828a5e68c2c6ce590.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86979a3d0ceaa90430ce",
+    "url": "/covid/dist/css/about.3653131e.css"
+  },
+  {
+    "revision": "5fdf8084bca7ff71f23f",
+    "url": "/covid/dist/css/app.3c0e2d8c.css"
+  },
+  {
+    "revision": "f6027b4d84ecbf28f949f7ea89f3771f",
+    "url": "/covid/dist/data.json"
+  },
+  {
+    "revision": "be603b7ea5b5eddca2f4bde7c459c3b9",
+    "url": "/covid/dist/data/bright.json"
+  },
+  {
+    "revision": "9fdca94ef7b6bb7f63799d2fc5341e66",
+    "url": "/covid/dist/data/countries.json"
+  },
+  {
+    "revision": "a1c786ee43560cc7136a1230e08ac979",
+    "url": "/covid/dist/data/location-heading.svg"
+  },
+  {
+    "revision": "77719cf61563f538c13b9bd319f2c1c4",
+    "url": "/covid/dist/data/marker.png"
+  },
+  {
+    "revision": "f1ce2b6b98a5fc59acdccd13318af5bf",
+    "url": "/covid/dist/data/meteorites.csv"
+  },
+  {
+    "revision": "012d5397ff078f28b3abdec081aa66a2",
+    "url": "/covid/dist/index.html"
+  },
+  {
+    "revision": "86979a3d0ceaa90430ce",
+    "url": "/covid/dist/js/about.aa8f8d80.js"
+  },
+  {
+    "revision": "5fdf8084bca7ff71f23f",
+    "url": "/covid/dist/js/app.37c7bd5b.js"
+  },
+  {
+    "revision": "2695906e16068c072993",
+    "url": "/covid/dist/js/chunk-vendors.72129791.js"
+  },
+  {
+    "revision": "1f7e65502d66518bb3808c33027fdc61",
+    "url": "/covid/dist/manifest.json"
+  },
+  {
+    "revision": "f953897cc0400fc1fd0975ab8de20879",
+    "url": "/covid/dist/paste/lzma.js"
+  },
+  {
+    "revision": "4cabfe5acd27d1953e03d27066f53d97",
+    "url": "/covid/dist/paste/lzma_worker.js"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/covid/dist/robots.txt"
+  }
+]);
